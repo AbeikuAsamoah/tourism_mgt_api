9911ccd3336759ca6e24cf7a817f425f643aa0c3
Collins <collinsabeiku07@gmail.com>
2026-03-05 00:38:33 +0000

    Update users app urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserRegisterView, UserLoginView, UserDetailView
+from .views import UserRegisterView, UserLoginView
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='user_register'),
